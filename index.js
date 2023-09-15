4b777cd58ec8b347f8109653b42cc70275df0865
Shashank Raj <shashankraj3636@gmail.com>
2023-09-15 15:44:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const app = express();
+const path = require("path");
+const forms = require("./routes");
+const mongoose = require("mongoose");
+const csrf = require("csurf");
+const cookieParser = require("cookie-parser");
+app.use(express.static("client"));
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const csrfProtection = csrf({cookie:true});
+app.use(csrfProtection);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/client"));
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  if (req.cookies.victo_id) {
+    res.render("./home.ejs", { isLoggedIn: true, csrfToken: req.csrfToken() });
+  } else {
+    res.render("./home.ejs", { isLoggedIn: false , csrfToken: req.csrfToken()});
+  }
+});
+
+app.use("/users", forms);
+
+mongoose
+  .connect(
+    "mongodb+srv://petparadise:Petparadise@cluster0.zuw8xzo.mongodb.net/test"
+  )
+  .then((result) => {
+    app.listen(8000);
+    console.log("server started successfully on 8000 PORT!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ routes.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const Victims = require("./victims");
+const victims = require("./victims");
+
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+  res.render("./form.ejs", { login: true, csrfToken: req.csrfToken() });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("./form.ejs", { login: false , csrfToken: req.csrfToken()  });
+});
+
+router.get("/delete", (req, res) => {
+  res.render("./delete.ejs", { csrfToken: req.csrfToken() });
+});
+router.post("/delete", async (req, res) => {
+  const text = req.body.typetext;
+  console.log(text)
+  if (text === "Confirm to delete") {
+
+    await Victims.findByIdAndDelete(req.cookies.victo_id);
+    res.clearCookie('victo_id');
+    res.redirect("/");
+  } else {
+    res.render("./delete.ejs", { csrfToken: req.csrfToken() });
+  }
+  // res.render("./delete.ejs");
+});
+
+router.post("/auth", async (req, res) => {
+  if (req.body.confirmpassword) {
+    const user = await Victims.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    await user.save();
+    res.render("./form.ejs", { login: true ,csrfToken: req.csrfToken() });
+  } else {
+    const username = req.body.username;
+    const password = req.body.password;
+    const user = await victims.findOne({
+      username: username,
+      password: password,
+    });
+    console.log(user);
+    if (user) {
+      res.cookie("victo_id", user._id, {
+        maxAge: 60 * 60 * 1000,
+      });
+      req.user = true;
+      res.redirect("/");
+    } else {
+      res.render("./form.ejs", { login: true , csrfToken: req.csrfToken()});
+    }
+  }
+});
+module.exports = router;
